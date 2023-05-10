08de674df8ba2d1cf9721daeec968b43deeeaefc
Vitali Schumarov <vitalischumarov@Vitalis-MacBook-Air-2.fritz.box>
2023-05-10 11:20:16 +0200

    first commit

---
+++ TipCalculator/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Vitali Schumarov on 10.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
